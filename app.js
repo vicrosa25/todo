0f1ee3bb7c72c28a3e4b0baeb30337e5a1a1bc1a
Víctor Rosa <vicrosa25@gmail.com>
2019-09-20 13:29:47 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// Add todos
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+
+  list.innerHTML += html;
+};
+
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  // prevent empty todos
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+// Delete todos
+list.addEventListener('click', e => {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+});
